534989c74154d413cc531349cd6c177155ef573c
YashSomani18 <yashsomani18@gmail.com>
2023-12-03 14:42:44 +0530

    Fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
 export const YOUTUBE_VIDEO_API =
-  "https://corsproxy.io/?https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   GOOGLE_API_KEY;
 
 export const YOUTUBE_SEARCH_API =
